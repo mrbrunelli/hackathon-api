e9c6a9c11dd315939adbe190e9aa1728edcb91d4
Raquel <raquel.macedo.1195@gmail.com>
2021-06-15 20:13:28 -0300

    fix dashboard button feature

--- src/resources/views/layouts/master.blade.php
+++ src/resources/views/layouts/master.blade.php
@@ -80,7 +80,7 @@
         </div>
         <div class="d-block">
           <h2 class="h5 mb-3">Bem-vindo!</h2>
-          <a href="" class="btn btn-secondary btn-sm d-inline-flex align-items-center">
+          <a href="{{route('logout')}}" class="btn btn-secondary btn-sm d-inline-flex align-items-center">
             <svg class="icon icon-xxs me-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>            
             Sair
           </a>
@@ -104,7 +104,7 @@
         </a>
       </li>
       <li class="nav-item ">
-        <a href="#" class="nav-link">
+        <a href="{{ route('dashboard') }}" class="nav-link">
           <span class="sidebar-icon">
             <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
           </span> 
